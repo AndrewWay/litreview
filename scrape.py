c2de5aa26617be139dc37f9af75eabbd5b24d25d
Andrew Way <arw405@mun.ca>
2018-05-05 21:14:13 -0230

    Added Usage print statement at beginning of scrape

--- scrape.py
+++ scrape.py
@@ -2,22 +2,25 @@ from sys import argv
 from string import punctuation
 from collections import *
 from difflib import SequenceMatcher
+from isText import istext
 
+#Check if file names passed to script
 
-mz_keyWords = ['m/z','range','to']
+if((len(argv)<2)):
+    print("Usage: python scrape.py file1.txt file2.txt etc.txt")
 
 
 topics = []
 headers = []
 
-#TODO fix this initialization
+#TODO fix this initialization so that the key words are read from files
 
 #Year
 year_keyWords = ['2018','2017','2016']
 topics.append(year_keyWords)
 headers.append("Year")
 #Source
-source_keyWords = ['Waters','OmniSpray','Prosolia','Custom','custom','lab-built']
+source_keyWords = ['Waters','OmniSpray','Prosolia','Custom','custom','lab-built',"source","ion"]
 topics.append(source_keyWords)
 headers.append("Source")
 #MS
@@ -52,11 +55,90 @@ headers.append("N2 Pressure (PSI)")
 ionMode_keyWords = ["ion","mode","positive","negative"]
 topics.append(ionMode_keyWords)
 headers.append("Ion Mode")
+#injection
+injection_keyWords = ["injection","ms"]
+topics.append(injection_keyWords)
+headers.append("Injection ms")
+#Microscans
+microscans_keyWords = ["microscan","microscans"]
+topics.append(microscans_keyWords)
+headers.append("Microscans")
+#Spray voltage
+sprayVoltage_keyWords = ["spray","voltage","V","v","kV","volts"]
+topics.append(sprayVoltage_keyWords)
+headers.append("Spray voltage kV")
+#Capillary voltage
+capillaryVoltage_keyWords = ["capillary","voltage","V","v","kV","volts","cap"]
+topics.append(capillaryVoltage_keyWords)
+headers.append("Capillary Voltage V")
+#Tube lens
+tubeLens_keyWords = ["tube","lens","voltage","V","kV"]
+topics.append(tubeLens_keyWords)
+headers.append("Tube Lens V")
+#Capillary Temperature
+capillaryTemperature_keyWords = ["capillary","temperature","C","°C","°"]
+topics.append(capillaryTemperature_keyWords)
+headers.append("Capillary Temperature °C")
+#m/z range
+mzRange_keyWords = ["m/z","range","to"]
+topics.append(mzRange_keyWords)
+headers.append("m/z range")
+#Resolution
+resolution_keyWords = ["pixel","resolution","um"]
+topics.append(resolution_keyWords)
+headers.append("Resolution um")
+#Resolving Power
+resolvingPower_keyWords = ["resolving","power","kV"]
+topics.append(resolvingPower_keyWords)
+headers.append("Resolving Power kV")
+#Length
+length_keyWords = ["length","m","mm","cm"]
+topics.append(length_keyWords)
+headers.append("Length mm")
+#Line Seperation
+lineSeperation_keyWords = ["line","seperation","mm","cm","step","size"]
+topics.append(lineSeperation_keyWords)
+headers.append("Line Seperation mm")
+#Step 
+step_keyWords = ["step","x-axis","axis","size"]
+topics.append(step_keyWords)
+headers.append("Step mm")
+#Scan duration
+scanDuration_keyWords = ["scan","duration","time","ms"]
+topics.append(scanDuration_keyWords)
+headers.append("Scan duration ms")
+#Velocity
+velocity_keyWords = ["velocity","rate","um/sec","um/s","mm/s"]
+topics.append(velocity_keyWords)
+headers.append("Velocity um/s")
+#Substrate
+substrate_keyWords = ["substrate","slide","glass"]
+topics.append(substrate_keyWords)
+headers.append("Substrate")
+#Acquisition Software
+acquisitionSoftware_keyWords = ["software","acquisition","Xcalibur","data","MassLynx"]
+topics.append(acquisitionSoftware_keyWords)
+headers.append("Acquisition Software")
+#Conversion Software
+conversionSoftware_keyWords = ["software","conversion","convert","process","processing","raw","firefly","FireFly"]
+topics.append(conversionSoftware_keyWords)
+headers.append("Conversion software")
+#Visualization Software
+visualizationSoftware_keyWords = ["software","visualization","visualize","visual","image","images","processed"]
+topics.append(visualizationSoftware_keyWords)
+headers.append("Visualization Software")
 
 
+output_file = "output.txt"
 
+def append(output):
+  with open(output_file,"a") as myfile:
+    myfile.write(output)
+    
+append("fuck off")
 #For each file in the arguments
 for one_filename in argv[1:]:
+
   output_string=""
   #Open the file and read its contents
   print("Text file to import and read:", one_filename)
@@ -88,13 +170,14 @@ for one_filename in argv[1:]:
             matchFrequency = 1
           else:
             matchness[-1]+=1
-          
-    # Sort the lines based on how many keywords they contain in ascending order
-    matchness,matches = zip(*sorted(zip(matchness,matches)))
     
-    matchness,matches = (list(t) for t in zip(*sorted(zip(matchness,matches))))
-    matchness = matchness[::-1]
-    matches = matches[::-1]
+    if(len(matches)>0):      
+      # Sort the lines based on how many keywords they contain in ascending order
+      matchness,matches = zip(*sorted(zip(matchness,matches)))
+    
+      matchness,matches = (list(t) for t in zip(*sorted(zip(matchness,matches))))
+      matchness = matchness[::-1]
+      matches = matches[::-1]
     
     # USER PROCESSING SECTION
     
@@ -105,6 +188,7 @@ for one_filename in argv[1:]:
     while(exit and i<len(matches)):
       print(matchness[i],matches[i])
       user_input = input()
+      #maybe make this into a function that can call itself
       if not user_input:
         pass# Do nothing. Just go to next string
       else:
@@ -113,7 +197,7 @@ for one_filename in argv[1:]:
         output_string = output_string + "," + user_input
         # MOVE TO NEXT COLUMN
       i+=1
-      if(i>=len(matches)):
+      if(i>=len(matches) and exit):
         print("No more matches. Put in best guess?")
         user_input = input()
         if not user_input:
@@ -123,7 +207,6 @@ for one_filename in argv[1:]:
           # ENTER STRING INTO APPROPRIATE SPREADSHEET CELL
           output_string = output_string + "," + user_input
           # MOVE TO NEXT COLUMN
-  print(headers)
-  print(output_string)
+  append(output_string)
     
     
